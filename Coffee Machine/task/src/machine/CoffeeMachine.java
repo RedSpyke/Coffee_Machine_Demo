cc54c5547efff581c750903c2aceb03a04ff73e3
Alexandru Palcau <alexandrupalcau@gmail.com>
2020-06-27 12:48:38 +0300

    Stage 4 src

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -8,51 +8,57 @@ public class CoffeeMachine {
     static int Milk_For_One_Cup = 50;
     static int Coffee_Grounds_For_One_Cup = 15;
 
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int waterAvailable;
-        int milkAvailable;
-        int coffeeGroundsAvailable;
+        CoffeeMaker machine = new CoffeeMaker(400,540,120,550,9);
 
+        machine.coffeeMachineState();
+        System.out.println("Write action (buy, fill, take): ");
+        String action = input.next();
+        switch (action){
+            case "buy":
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+                int choice = input.nextInt();
+                switch (choice){
+                    case 1:
+                        machine.makeEspresso();
+                        break;
+                    case 2:
+                        machine.makeLatte();
+                        break;
+                    case 3:
+                        machine.makeCappuccino();
+                        break;
+                    default:
+                        //   System.out.println("Invalid option");
+                }
 
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
-
-        System.out.println("Write how many ml of water the coffee machine has: ");
-        waterAvailable = input.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has: ");
-        milkAvailable = input.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
-        coffeeGroundsAvailable = input.nextInt();
-        System.out.println("Write how many cups of coffee you will need: ");
-        int numberOfCoffees = input.nextInt();
-
-        if (coffeeCupsAvailable(waterAvailable,milkAvailable,coffeeGroundsAvailable) >= numberOfCoffees){
-            int extraCoffee = coffeeCupsAvailable(waterAvailable,milkAvailable,coffeeGroundsAvailable) - numberOfCoffees;
-            System.out.println("Yes, I can make that amount of coffee (and even " + extraCoffee + " more than that)");
+                break;
+            case "fill":
+                machine.fillMachine();
+                break;
+            case "take":
+                machine.takeMoney();
+                break;
+            default:
+             //   System.out.println("Invalid option");
         }
-        else if(canMakeRequestedCoffee(waterAvailable, milkAvailable,coffeeGroundsAvailable,numberOfCoffees)){
-            System.out.println("Yes, I can make that amount of coffee");
-        }else {
-            System.out.println("No, I can make only " + coffeeCupsAvailable(waterAvailable,milkAvailable,coffeeGroundsAvailable) + " cup(s) of coffee");
-        }
+
+
 
     }
+
+
 
     public static void getCoffeeNeeds (int numberOfCoffees){
         System.out.println("For "+ numberOfCoffees + " cups of coffee you will need:");
         System.out.println(numberOfCoffees * Water_For_One_Cup + " ml of water ");
-        System.out.println(numberOfCoffees * + Milk_For_One_Cup + " ml of milk ");
+        System.out.println(numberOfCoffees * Milk_For_One_Cup + " ml of milk ");
         System.out.println(numberOfCoffees * Coffee_Grounds_For_One_Cup + " g of coffee beans");
     }
-    public static boolean canMakeRequestedCoffee (int water, int milk, int coffee , int numberOfCoffees ){
+
+    public static boolean canMakeRequestedCoffee (int water, int milk, int coffee ,int numberOfCoffees){
        boolean canMakeCoffee = false;
         if((water - (numberOfCoffees * Water_For_One_Cup)) >= 0 &&
                (milk - (numberOfCoffees * Milk_For_One_Cup) >= 0) &&
